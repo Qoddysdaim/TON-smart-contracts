eaed1b065446e81437643f0641323458c0dd3701
Qoddrysdaim <khanbekov.blockchain@gmail.com>
2021-11-12 03:47:34 +0300

    Add shopping list debot

---
+++ contracts/8_DeBot/ShoppingDebot/ShoppingList.sol
@@ -0,0 +1,86 @@
+pragma ton-solidity >=0.47.0;
+pragma AbiHeader expire;
+pragma AbiHeader time;
+pragma AbiHeader pubkey;
+
+import "Structures.sol";
+
+contract ShoppingList is PurchasesContainer{
+
+    uint ownerPubKey;
+    uint nextPurchaseId;
+    mapping(uint => Purchase) idToPurchaseMapping;
+
+    constructor(uint pubkey) public {
+        require(pubkey != 0, 120);
+        tvm.accept();
+        nextPurchaseId = 0;
+        ownerPubKey = pubkey;
+    }
+
+    modifier onlyOwner(){
+        require(msg.pubkey() == ownerPubKey, 200);
+        _;
+    }
+
+    function addToPurchasesList(string name, uint amount) public override onlyOwner{
+        tvm.accept();
+        nextPurchaseId++;
+        idToPurchaseMapping[nextPurchaseId] = Purchase(nextPurchaseId, name, amount, now, false, 0, false);
+    }
+
+    function deleteFromPurchasesList(uint id) public override onlyOwner{
+        require(!idToPurchaseMapping[id].deleted, 104);
+        if(idToPurchaseMapping.exists(id)){
+            tvm.accept();
+            idToPurchaseMapping[id].deleted = true;
+        }
+    }
+
+    function buy(uint id, uint price) public override onlyOwner{
+        optional(Purchase) purchaseToBuy = idToPurchaseMapping.fetch(id);
+        require(purchaseToBuy.hasValue(), 103);
+        require(!idToPurchaseMapping[id].deleted, 104);
+        tvm.accept();
+        idToPurchaseMapping[id].totalPrice = price;
+        idToPurchaseMapping[id].purchased = true;
+    }
+
+    function getPurchasesSummary() public override returns(PurchasesSummary){
+        tvm.accept();
+        PurchasesSummary summary;
+        for((uint id, Purchase purchaseToBuy) : idToPurchaseMapping){
+            if(!purchaseToBuy.deleted){
+                if(purchaseToBuy.purchased){
+                    summary.totalPaidAmount += purchaseToBuy.amount;
+                    summary.totalPaidPrice += purchaseToBuy.totalPrice;
+                } else{
+                    summary.totalNotPaidAmount += purchaseToBuy.amount;
+                } 
+            }
+        }
+        return summary;
+    }
+
+    function getPurchasesList() public override returns(Purchase[] purchasesList){
+        uint id;
+        string name;
+        uint amount;
+        uint whenCreated;
+        bool purchased;
+        uint totalPrice;
+        bool deleted;
+
+        for((uint _id, Purchase purchase) : idToPurchaseMapping) {
+            if(!purchase.deleted){
+                id = _id;
+                name = purchase.name;
+                amount = purchase.amount;
+                whenCreated = purchase.whenCreated;
+                purchased = purchase.purchased;
+                totalPrice = purchase.totalPrice;
+                purchasesList.push(Purchase(id, name, amount, whenCreated, purchased, totalPrice, false));
+            }
+       }
+    }
+}
---
+++ contracts/8_DeBot/ShoppingDebot/ShoppingListDebot.sol
@@ -0,0 +1,72 @@
+pragma ton-solidity >=0.47.0;
+pragma AbiHeader expire;
+pragma AbiHeader time;
+pragma AbiHeader pubkey;
+
+import "BaseListDebot.sol";
+import "ShoppingList.sol";
+
+contract ShoppingListDebot is BaseListDebot{
+
+    string private nameOfCurrentPurchase;
+
+    function showMenu(PurchasesSummary summary) public override{
+        string shoppingSummary = getFormattedShoppingSummary(summary);
+        Menu.select(shoppingSummary, "",
+            [
+                MenuItem("Add product to shopping list", "", tvm.functionId(addNameInput)),
+                MenuItem("Remove product from shopping list", "", tvm.functionId(removeIdInput)),
+                MenuItem("Show my shopping list", "", tvm.functionId(showShoppingList))
+            ]
+        );
+    }
+
+    function addNameInput(uint32 index) public{
+        index = index;
+        Terminal.input(tvm.functionId(addAmountInput), "Please, enter the name of your product", false);
+    }
+
+    function addAmountInput(string value) public{
+        nameOfCurrentPurchase = value;
+        Terminal.input(tvm.functionId(addToShoppingList), "Please, provide amount of items to purchase", false);
+    }
+
+    function addToShoppingList(string value) public{
+        (uint amount, bool valid) = stoi(value);
+        PurchasesContainer(contractAddress).addToPurchasesList {
+            abiVer: 2,
+            sign: true,
+            pubkey: userPubKey,
+            time: uint64(now),
+            expire: 0,
+            callbackId: tvm.functionId(onProductAdded),
+            onErrorId: tvm.functionId(onProductAddError)
+        }(nameOfCurrentPurchase, amount);
+    }
+
+    function onProductAdded() public{
+        Terminal.print(0, "You successfully added a new item in your shopping list");
+        showData();
+    }
+
+    function onProductAddError(uint32 sdkError, uint32 exitCode) public{
+        Terminal.print(0, "Error occured. Please, try again");
+        addNameInput(0);
+    }
+
+    function getDebotInfo() public functionID(0xDEB) override view returns(
+        string name, string version, string publisher, string caption, string author,
+        address support, string hello, string language, string dabi, bytes icon
+    ) {
+        name = "Shopping List DeBot";
+        version = "1.0.0";
+        publisher = "Qoddrysdaim";
+        author = "Qoddrysdaim";
+        support = address.makeAddrStd(0, 0x1e3713373c839489cd84f0745d7f98a0ba3bcdbd91a56ac30c79f769303ec603);
+        hello = "Welcome to Shopping list DeBot.";
+        language = "en";
+        dabi = m_debotAbi.get();
+        icon = iconPath;
+    }
+    
+}
